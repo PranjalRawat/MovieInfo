998add2cff9baf4e73ea5f6bdfacb67700db2429
PranjalRawat <rawatpranjal111@gmail.com>
2020-05-30 17:24:37 +0530

    added movie

--- movie_info/settings/local.py
+++ movie_info/settings/local.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '@x9^gbt7_u^v_dvxmfs2ek4)g+w1p&#w47*8r43xb%qp$3z%1u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1","webmovieinfo.herokuapp.com"]
 
